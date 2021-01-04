c8ee869bb0617bbb2cd510deb66429ed2db353b7
Belmiro <tengku13elmiro@gmail.com>
2021-01-04 13:45:29 +0700

    Simple map

--- app/src/main/java/com/suncode/examplegooglemap/MainActivity.java
+++ app/src/main/java/com/suncode/examplegooglemap/MainActivity.java
@@ -1,14 +1,38 @@
 package com.suncode.examplegooglemap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        map = googleMap;
+
+        LatLng bandung = new LatLng(-6.917731498045556, 107.6156894322087);
+        map.addMarker(new MarkerOptions().position(bandung).title("Bandung city"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(bandung));
     }
 }
